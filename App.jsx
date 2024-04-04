8a7a4fd20f09e197468ab1c2538b911488a8f500
Rua <rua.elkasheef@gmail.com>
2024-04-04 19:01:10 -0400

    library-react-exercise

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Nav from "./components/Nav.jsx" ;
+
+function App() {
+  return (
+    <div className='App' >
+      <Nav></Nav>
+    </div>
+  );
+}
+export default App;
