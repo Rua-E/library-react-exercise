3abef207ace93efb9829c348bbc13092c948e361
Rua <rua.elkasheef@gmail.com>
2024-04-06 18:43:38 -0400

    creating discounted page

--- components/ui/Book.jsx
+++ components/ui/Book.jsx
@@ -21,11 +21,11 @@ const Book = ({ book }) => {
         </div>
         <div className="book__ratings">
             {
-                new Array(book.rating).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index}/>)
+                new Array(Math.floor(book.rating)).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index}/>)
             }
-         
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star-half-alt" />
+            {
+              !Number.isInteger(book.rating) && <FontAwesomeIcon icon="star-half-alt" /> 
+            }
         </div>
         <div className="book__price"> 
             {book.salePrice ? (
