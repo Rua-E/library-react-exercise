89508cd00f8febc139664eda9a31722a0f6689ba
Rua <rua.elkasheef@gmail.com>
2024-04-06 17:59:39 -0400

    completed featured books section

--- components/ui/Book.jsx
+++ components/ui/Book.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Book = () => {
+const Book = ({ book }) => {
   return (
     <>
       <div className="book">
         <a href="">
           <figure className="book__img--wrapper">
             <img
-              src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
+              src={book.url}
               alt=""
               className="book__img"
             />
@@ -16,20 +16,25 @@ const Book = () => {
         </a>
         <div className="book__title">
           <a href="" className="book__title--link">
-            Atomic Habits
+            {book.title} 
           </a>
         </div>
         <div className="book__ratings">
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
+            {
+                new Array(book.rating).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index}/>)
+            }
+         
           <FontAwesomeIcon icon="star" />
           <FontAwesomeIcon icon="star-half-alt" />
         </div>
-        <div className="book__price">
-          <span className="book__price--normal">$15.00</span>
-          $10.00
+        <div className="book__price"> 
+            {book.salePrice ? (
+            <>
+                <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
+                ${book.salePrice.toFixed(2)} 
+            </> ) : 
+            <>{book.originalPrice.toFixed(2)}</>
+            }
         </div>
       </div>
     </>
